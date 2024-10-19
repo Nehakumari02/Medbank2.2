5d68cf5d82e683631b932511680c37790760585a
nehakumari02 <b20115@students.iitmandi.ac.in>
2024-10-19 18:39:34 +0530

    confirmation email for inquiry

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -18,8 +18,35 @@ export async function POST(req) {
     text: `Name: ${name}\nPhone Number: ${phoneNumber}\nEmail: ${email}\nInquiry Details: ${inquiryDetails}`,
   };
 
+  const mailOptions1 = {
+    from: email,
+    to: process.env.EMAIL_RECEIVER, // Your email address
+    subject: "お問い合わせありがとうございます（Thank you very much for your inquiry）",
+    text: `
+          <p>この度はMEDBANKの遺伝子解析サービスへのお問い合わせありがとうございました。</p>
+          <p>Thank you very much for your inquiry to our gene sequencing service.</p>
+
+          <p>下記の内容で情報を受け付けました。</p>
+          <p>Information was accepted on the following:</p>
+
+          <ul>
+            <li><strong>Name</strong>: ${name}</li>
+            <li><strong>Phone Number</strong>: ${phoneNumber}</li>
+            <li><strong>Email</strong>: ${email}</li>
+            <li><strong>Inquiry Details</strong>: ${inquiryDetails}</li>
+          </ul>
+
+          <p>お問い合わせ内容は2日以内を目安にお返事致しております。各オーダーについてや、お急ぎの場合は、マイページのチャットからお問い合わせください。</p>
+          <p>Our reply to your inquiry will be within 2 days. If you have any questions about each order or if you are in a hurry, please contact us through the chat on My Page.</p>
+
+          <p>Best regards,<br>The MEDBANK Team</p>
+        `
+    
+  }
+
   try {
     await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions1);
     return new Response(JSON.stringify({ message: 'Email sent successfully' }), {
       status: 200,
     });
